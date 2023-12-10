7b24ee8c349f1a48195109369313e0e2af90be05
David Heidari <davidheidari2@gmail.com>
2023-12-10 20:22:32 +0100

    removed a button and do all function in an input

--- js/links.js
+++ js/links.js
@@ -1,10 +1,11 @@
 
 const linkInput = document.querySelector('#linkInput');
-const quickLinkButton = document.querySelector('.quick-links-button');
+//const quickLinkButton = document.querySelector('.quick-links-button');
 const ulList = document.querySelector('.ulList');
 
-quickLinkButton.addEventListener('click', (e) => {
-    const linkValue = linkInput.value.trim();
+/*
+linkInput.addEventListener('click', (e) => {
+    const linkValue = linkInput.value;
         
     if (linkValue !== '') {
         const li = document.createElement('li');
@@ -24,13 +25,32 @@ quickLinkButton.addEventListener('click', (e) => {
         linkInput.value = '';
     }
 });
+*/
 
 linkInput.addEventListener('mouseout', (e) => {
         // Hantera händelsen när musen går ut från linkInput här, om nödvändigt
 });
+linkInput.addEventListener('click', (e) => { 
+    const linkValue = linkInput.value;
+        
+    if (linkValue !== '') {
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+
+        a.href = linkValue;
+        a.textContent = linkValue;
+        a.target = '_blank';
+
+        a.addEventListener('click', (event) => {
+            event.preventDefault();
+            window.open(linkValue, '_blank');
+        });
+
+        li.appendChild(a);
+        ulList.appendChild(li);
+        linkInput.value = '';
+    }
+})
 
 
-function getLink() {
-     
-}
     
