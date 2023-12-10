66740172c953142a020e4d39f3ffdeb5b8adcd3f
David Heidari <davidheidari2@gmail.com>
2023-12-10 14:39:19 +0100

    added quickLink js-code

--- js/links.js
+++ js/links.js
@@ -1,2 +1,36 @@
-const quicklinks = document.querySelector('.quick-links');
 
+const linkInput = document.querySelector('#linkInput');
+const quickLinkButton = document.querySelector('.quick-links-button');
+const ulList = document.querySelector('.ulList');
+
+quickLinkButton.addEventListener('click', (e) => {
+    const linkValue = linkInput.value.trim();
+        
+    if (linkValue !== '') {
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+
+        a.href = linkValue;
+        a.textContent = linkValue;
+        a.target = '_blank';
+
+        a.addEventListener('click', (event) => {
+            event.preventDefault();
+            window.open(linkValue, '_blank');
+        });
+
+        li.appendChild(a);
+        ulList.appendChild(li);
+        linkInput.value = '';
+    }
+});
+
+linkInput.addEventListener('mouseout', (e) => {
+        // Hantera händelsen när musen går ut från linkInput här, om nödvändigt
+});
+
+
+function getLink() {
+     
+}
+    
