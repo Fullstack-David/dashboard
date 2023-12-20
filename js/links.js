fa3a91ba06d58d92d57568d2b476612d8802f045
David Heidari <davidheidari2@gmail.com>
2023-12-20 23:26:36 +0100

    added some js file and css

--- js/links.js
+++ js/links.js
@@ -1,24 +1,30 @@
 const quickLinks = document.querySelector('.quick-links')
 const linkInput = document.getElementById('linkInput');
 const ulList = document.querySelector('.ulList');
-/*
-// click event 
-linkInput.addEventListener('click', (e) => {
-    addItem()
-});
-*/
 
 // Enter event 
 linkInput.addEventListener('keypress', (e) => {
     if (e.key === 'Enter') {
-        addItem()
-        console.log(linkInput)
-    } else {
-        console.log('Error')
-    }
+        addItem();       
+    } 
 });
 
+/* Lyssna på när dokumentet är helt laddat och 
+redo för manipulation med JavaScript */
 
+document.addEventListener('DOMContentLoaded', function () {
+    getquickLinks();
+});
+
+function getquickLinks() {
+    let fastLinks = JSON.parse(localStorage.getItem('link'));
+    
+    fastLinks.forEach(link => {
+        displayLocal(link);
+    });  
+}
+
+linkList = [];
 
 function addItem() {
 
@@ -28,28 +34,71 @@ function addItem() {
     const delBtn = document.createElement('button');
     delBtn.textContent = 'X';
 
-
-    // ger mina element ett class namn
-    delBtn.className = 'delBtn';
-    a.className = 'link';
+    // ger mina element ett class namn    
     li.className = 'list-group-item';
     a.href = linkInput.value;
-
-    // skall cärdet på li synas på skärmen
+    a.className = 'link';
+    delBtn.className = 'delBtn';
+    
+    // skall värdet på li synas på skärmen
     li.textContent = a.value;
-    //console.log(linkInput)
-    console.log(a);
-    a.textContent = linkInput.value;
+    a.textContent = linkInput.value; 
 
-    //appendar
-    li.appendChild(delBtn);
+    // appendar    
     ulList.appendChild(li);
     li.appendChild(a);
+    li.appendChild(delBtn);
     
     // event på knappen 
     delBtn.addEventListener('click', (e) => {
         delBtn.parentElement.remove();
     });
+    
+    linkList.push(linkInput.value);
+
+    // localStorage
+    localStorage.setItem('link', JSON.stringify(linkList));
+    
+    // tömmer input
+    linkInput.value = ''; 
 }
+
+function displayLocal(link) {
+     // skapar element inut min quick-link
+     const li = document.createElement('li');
+     const a = document.createElement('a');
+     const delBtn = document.createElement('button');
+     delBtn.textContent = 'X';
+ 
+     // ger mina element ett class namn    
+     li.className = 'list-group-item';
+     a.href = link;
+     a.className = 'link';
+     delBtn.className = 'delBtn';
+    
+
+     delBtn.value = link;     
+     a.textContent = link; 
+ 
+     // appendar    
+     ulList.appendChild(li);
+     li.appendChild(a);
+     li.appendChild(delBtn);
+     
+     // event på knappen 
+    delBtn.addEventListener('click', (e) => {
+        delBtn.parentElement.remove();
+        linkList = JSON.parse(localStorage.getItem('link'));
+        console.log(linkList)
+        linkList = linkList.filter((link) => {
+            return link !== e.target.value;            
+        });
+        localStorage.setItem('link', JSON.stringify(linkList));
+    });     
+}
+
+
+
+
 
 
---
+++ js/myFav.js
@@ -0,0 +1,53 @@
+let url = 'http://www.boredapi.com/api/activity?type=recreational';
+
+fetch(url)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok.');                
+        }
+        return response.json();             
+    })
+    .then(data => {
+        const boredApi = document.querySelector('.bored-api');
+        
+        let para = data.activity;
+        let participants = data.participants;
+        let price = data.price;
+        let link = data.link;
+        let aLink;
+
+        console.log("Data: ", data);
+
+        if (link === '') {
+            aLink = ''
+        } else {
+            aLink = `<p><strong></strong><a href="${link}">Klick here to see how its work!</a></p>`
+        }
+
+        boredApi.innerHTML = `
+        <div class = 'header'>
+            <h2>The Bored API</h2>
+            <h4>Let's find you something to do</h4>
+        </div>
+        <div class="boredInput">         
+          <p><strong>Activity:</strong> ${para}</p>
+          <p><strong>Participants:<strong> ${participants}</p>
+          <p><strong>Price:</strong> ${price} $</p>
+          ${aLink}
+        </div>
+        `
+        
+    })
+
+    .catch(error => {
+        console.error('Error', error);
+    })
+   
+
+
+
+
+
+
+
+
