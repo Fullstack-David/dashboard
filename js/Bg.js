0cb018c1d106d501aae040e4ca6ba5b9321f18b4
David Heidari <davidheidari2@gmail.com>
2023-12-25 22:39:10 +0100

    added default background image to my function

---
+++ js/Bg.js
@@ -0,0 +1,59 @@
+// Säkerställer att min JavaScript inte körs förräns DOM är helt laddad.
+document.addEventListener('DOMContentLoaded', function () {
+    const apiKey = 'zssxLbwBAssRvlJa066O2P40u9brHdNtHQfY3oqAQjg';
+    const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}`;
+    const changeBackground = document.querySelector('.change-background');
+    const changeBackgroundBtn = document.querySelector('.change-background-btn');
+    const backgrundImg = document.querySelector('.backgrundImg');
+
+
+    function setDefaultBackground() {
+        const setDefaultBackgroundUrl = './assets/bg.jpg';
+        document.body.style.backgroundImage = `url(${setDefaultBackgroundUrl})`
+    }
+
+
+
+    function fetchAndSetBackground() {
+    
+        // Fetchar min url
+        fetch(apiUrl)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok.'); // Om reponsen inte är okej så ska en error kastas med innehåll                
+                }
+                return response.json(); // retunerar  den parsade JSON-data som en promise           
+            })
+            .then(data => {
+                const imgUrl = data.urls.regular; // skapar varibel imgUrl och tilldelar värdet 'regular' i data-objektet
+                document.body.style.backgroundImage = `url(${imgUrl})`; // ändrar body.background med imgUrl-värdet
+            })
+            .catch(error => console.error('Error:', error)); // Om något inte funkar så fångar jag den med catch-funktion
+    };
+
+    // EventListener på min reload image
+    /*
+    function setDefaultBackground() {
+        fetch(apiUrl)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok.');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log(data)
+                const imgUrl = data.urls.regular;
+                document.body.style.backgroundImage = `url(${imgUrl})`;
+               
+                console.log(imgUrl)
+            })
+            .catch(error => console.error('Error:', error));
+    }
+    */
+    setDefaultBackground();
+    changeBackgroundBtn.addEventListener('click', fetchAndSetBackground);
+    backgrundImg.addEventListener('click', fetchAndSetBackground)   
+})
+
+
